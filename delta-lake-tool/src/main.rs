04d695eac96e7f7163050298b8f25112f8170948
Roman Zanko <roman.zanko.vl@gmail.com>
2025-10-23 23:15:17 +0400

    Add support for reading data from Delta tables using DataFusion

--- delta-lake-tool/src/main.rs
+++ delta-lake-tool/src/main.rs
@@ -1,9 +1,12 @@
+use datafusion::arrow;
 use deltalake::protocol::SaveMode;
 use deltalake::writer::{DeltaWriter, JsonWriter};
 use deltalake::{DataType, DeltaOps, PrimitiveType};
+use deltalake::datafusion::prelude::SessionContext;
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::sync::Arc;
 use url::Url;
 
 #[tokio::main]
@@ -13,8 +16,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dir.push("http_requests");
     let url = Url::parse(format!("file://{}", dir.display()).as_str())?;
 
-    //create_table(url.clone()).await?;
+    create_table(url.clone()).await?;
     write_data(url.clone()).await?;
+    read_data(url.clone()).await?;
+
+    Ok(())
+}
+
+async fn read_data(url: Url) -> Result<(), Box<dyn std::error::Error>> {
+    let table = deltalake::open_table(url).await?;
+
+    let ctx = SessionContext::new();
+    ctx.register_table("http_requests", Arc::new(table))?;
+    let results = ctx
+        .sql("SELECT * FROM http_requests")
+        .await?
+        .collect()
+        .await?;
+    let pretty_results = arrow::util::pretty::pretty_format_batches(&results)?.to_string();
+
+    println!("{}", pretty_results);
 
     Ok(())
 }
